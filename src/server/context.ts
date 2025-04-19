d8df7d6693ad705e433189f79a4d42157dd778af
antonyoandrei <antonyoandrei7@gmail.com>
2025-04-19 16:14:47 +0200

    deployment context bug fix

--- src/server/context.ts
+++ src/server/context.ts
@@ -2,7 +2,7 @@
  * Contexto de cada petición tRPC.
  * Aquí podríamos inyectar session, prisma, servicios externos, etc.
  */
-export type Context = {};
+export type Context = object;
 
 // En esta prueba devolvemos vacío, pero sirve de hook para lógica auth/BD.
 export const createContext = async (): Promise<Context> => ({});
